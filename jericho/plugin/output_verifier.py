4f8409ba3da2f4404f1bca2e999cb6e1e9f01961
Ditt Namn <du@example.com>
2021-12-05 20:53:58 +0100

    Removed redundant code

--- jericho/plugin/output_verifier.py
+++ jericho/plugin/output_verifier.py
@@ -141,7 +141,7 @@ class OutputVerifier:
 
     def _is_json(self, content: str) -> bool:
         """Private method to attempt a json parse"""
-        if content[0].lstrip() != "{":
+        if content[0] != "{":
             return False
 
         return True
