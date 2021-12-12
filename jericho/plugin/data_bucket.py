19c10977d2f12748ede67353ae2df06b8171a542
Ditt Namn <du@example.com>
2021-12-12 14:06:14 +0100

    Changed getsizeof to json serialization and len because memory allocation won't be the same as the payload size

--- jericho/plugin/data_bucket.py
+++ jericho/plugin/data_bucket.py
@@ -1,37 +1,43 @@
 import logging
-from sys import getsizeof
-
+import typing
+import json
 
 class DataBucket:
-    def __init__(self, max_size):
-        logging.debug(f"Setting the data bucket size to {max_size}")
-        self.max_size = max_size
-        self.bucket = []
+    def __init__(self, max_size: int):
+        logging.debug("Setting the data bucket size to %s", max_size)
+        self.max_size: int = max_size
+        self.bucket_size: int = 0
+        self.bucket: typing.List = []
 
-    def save(self, data_to_save: tuple):
-        logging.info(f"Saving {getsizeof(data_to_save)} bytes to bucket ({getsizeof(self.bucket)} total size)")
+    def save(self, data_to_save: typing.Any) -> bool:
+        dict_size: int = len(json.dumps(data_to_save))
+        self.bucket_size = self.bucket_size + dict_size
+
+        logging.debug("Saving %s bytes to bucket (%s) max size", dict_size, self.max_size)
         self.bucket.append(data_to_save)
         return True
 
-    def get(self):
+    def get(self) -> typing.List:
         logging.debug("Getting data from the bucket")
         return self.bucket
 
-    def empty(self):
-        logging.debug("Emptying bucket")
+    def empty(self) -> bool:
+        logging.debug("Emptying bucket - current size (%s)", self.bucket_size)
         self.bucket = []
+        self.bucket_size  = 0
         return True
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         logging.debug("Checking if bucket is empty")
         return self.bucket == []
 
-    def is_full(self):
-        bucket_is_full = getsizeof(self.bucket) >= self.max_size
-        logging.debug(f"Checking if bucket is full: {bucket_is_full}")
+    def is_full(self) -> bool:
+        bucket_is_full = self.bucket_size >= self.max_size
+        logging.debug("Checking if bucket is full: %s", bucket_is_full)
         return bucket_is_full
 
-    def get_size(self):
-        size = getsizeof(self.bucket)
-        logging.debug(f"Getting size of data bucket: {size}")
-        return size
+    def get_size(self) -> int:
+        logging.debug("Getting size of data bucket: %s", self.bucket_size)
+        # Get the calculated bucket size = All the object sizes + the len of the json objects minus one
+        # (Representing the commas joining the objects + beginning and ending bracket)
+        return self.bucket_size + (len(self.bucket)-1) + 2
