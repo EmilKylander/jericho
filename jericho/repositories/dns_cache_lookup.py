26c3840ca40eb550a6dafb23f5e1c6da01e1a8a4
EmilKylander <45802059+EmilKylander@users.noreply.github.com>
2022-05-31 22:43:19 +0200

    Added context manager

--- jericho/repositories/dns_cache_lookup.py
+++ jericho/repositories/dns_cache_lookup.py
@@ -21,33 +21,26 @@ class DnsCacheLookup:
     async def find_ip(self, domain: str) -> typing.Optional[str]:
         """Check if a domain exist and if so get the content"""
         try:
-            cursor = await self.db.execute("SELECT * FROM jericho_dns_cache_lookup WHERE domain=?", (domain, ))
-            rows = await cursor.fetchall()
-            if len(rows) == 0:
-                return None
+            async with self.db.execute("SELECT * FROM jericho_dns_cache_lookup WHERE domain=?", (domain, )) as cursor:
+                rows = await cursor.fetchall()
+                if len(rows) == 0:
+                    return None
 
-            return rows[0][1]
+                return rows[0][1]
         except Exception as error:
-            logging.error("Could not find ip because of query error")
-        finally:
-            cursor.close()
+            logging.error("Could not find ip because of error %s", error)
 
     async def save(self, domain: str, ip_address: str) -> bool:
         """Save the ip address of a domain"""
         try:
             # Using aiosqlite for inserts because we are in an event loop right now and should therefore optimize the speed
-            cursor = await self.db.execute(
-                "INSERT OR IGNORE INTO jericho_dns_cache_lookup(domain, ip_address) VALUES(?, ?)",
-                (domain, ip_address, ),
-            )
-            await self.db.commit()
-            return True
+            async with self.db.execute("INSERT OR IGNORE INTO jericho_dns_cache_lookup(domain, ip_address) VALUES(?, ?)", (domain, ip_address, )) as cursor:
+                await self.db.commit()
+                return True
         except Exception as error:
             logging.error("Could not save ip address of domain %s because of error %s", domain, error)
             await self.db.rollback()
             return False
-        finally:
-            await cursor.close()
 
     async def close(self):
         await self.db.close()
