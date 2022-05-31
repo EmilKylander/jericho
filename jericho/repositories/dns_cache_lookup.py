efa63a176c5386679fa5f1455d09f17b1182eebe
EmilKylander <45802059+EmilKylander@users.noreply.github.com>
2022-05-31 23:00:56 +0200

    added retry

--- jericho/repositories/dns_cache_lookup.py
+++ jericho/repositories/dns_cache_lookup.py
@@ -20,23 +20,26 @@ class DnsCacheLookup:
 
     async def find_ip(self, domain: str) -> typing.Optional[str]:
         """Check if a domain exist and if so get the content"""
-        try:
-            async with self.db.execute("SELECT * FROM jericho_dns_cache_lookup WHERE domain=?", (domain, )) as cursor:
-                rows = await cursor.fetchall()
-                if len(rows) == 0:
-                    return None
+        attempt = 0
+        while True:
+            try:
+                async with self.db.execute("SELECT * FROM jericho_dns_cache_lookup WHERE domain=?", (domain, )) as cursor:
+                    rows = await cursor.fetchall()
+                    if len(rows) == 0:
+                        return None
 
-                return rows[0][1]
-        except Exception as error:
-            logging.error("Could not find ip because of error %s", error)
+                    return rows[0][1]
+            except Exception as error:
+                logging.error("Could not find ip because of error %s - attempt %s", error, attempt)
+                attempt = attempt + 1
 
     async def save(self, domain: str, ip_address: str) -> bool:
         """Save the ip address of a domain"""
         try:
             # Using aiosqlite for inserts because we are in an event loop right now and should therefore optimize the speed
-            async with self.db.execute("INSERT OR IGNORE INTO jericho_dns_cache_lookup(domain, ip_address) VALUES(?, ?)", (domain, ip_address, )) as cursor:
-                await self.db.commit()
-                return True
+            await self.db.execute("INSERT OR IGNORE INTO jericho_dns_cache_lookup(domain, ip_address) VALUES(?, ?)", (domain, ip_address, ))
+            await self.db.commit()
+            return True
         except Exception as error:
             logging.error("Could not save ip address of domain %s because of error %s", domain, error)
             await self.db.rollback()
